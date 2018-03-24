1fa0e05033f8378cc68bf0fd2af1f832c9aaf6f2
ronald hecker <ronald.m.hecker@gmail.com>
2018-03-24 17:01:51 +0100

    Working ash movement, still needs tuning

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+	private enum Direction
+	{
+		Up = 0,
+		Right = 1,
+		Down = 2,
+		Left = 3
+	}
+
+	private Direction _direction = Direction.Up;
+	private bool _moving = false;
+	private float _sinceStartOfDirection = 0.0f;
+
+	private Vector2 _currentTile = new Vector2();
+	private Vector2 _designatedTile = new Vector2();
+
+	private Animator _animator;
+	private Vector2 _offset = new Vector2(0.5f, 0.7f);
+
+	private float _velocity = 2.0f;
+	private bool _wantToStop;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		_animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		var wantToMove = false;
+		if (Input.GetAxis("Horizontal") > 0.1) {
+			wantToMove = SetDirection(Direction.Right);
+		} else if (Input.GetAxis("Horizontal") < -0.1) {
+			wantToMove = SetDirection(Direction.Left);
+		}
+
+		if (Input.GetAxis("Vertical") > 0.1) {
+			wantToMove = SetDirection(Direction.Up);
+		} else if (Input.GetAxis("Vertical") < -0.1) {
+			wantToMove = SetDirection(Direction.Down);
+		}
+
+		if (wantToMove == false) {
+			_sinceStartOfDirection = 0.0f;
+		}
+
+
+		if (_sinceStartOfDirection > 0.1) {
+			_moving = true;
+			_designatedTile = _currentTile + DirectionToVector2(_direction);
+		}
+
+		if (_designatedTile != _currentTile) {
+			var distanceFromDesignatedTile = ((Vector2) transform.position - _offset - _designatedTile).magnitude;
+			if (distanceFromDesignatedTile < 0.05) {
+				transform.position = (Vector3)(_designatedTile + _offset);
+				_currentTile = _designatedTile;
+                _moving = false;
+			} else {
+				transform.position += (Vector3) DirectionToVector2(_direction) * _velocity * Time.fixedDeltaTime;
+				Debug.Log(Vector3.left * _velocity * Time.fixedDeltaTime);
+			}
+		}
+
+		
+
+		_animator.SetInteger("Direction", (int)_direction);
+		_animator.SetBool("Moving", _moving);
+
+	}
+
+	private bool SetDirection(Direction direction)
+	{
+		if (_designatedTile == _currentTile) {
+			_sinceStartOfDirection += Time.fixedDeltaTime;
+			_direction = direction;
+		}
+
+		return true;
+	}
+
+	private Vector2 DirectionToVector2(Direction direction)
+	{
+        switch (_direction) {
+            case Direction.Left:
+                return Vector2.left;
+            case Direction.Right:
+                return Vector2.right;
+            case Direction.Up:
+                return Vector2.up;
+            default:
+	            return Vector2.down;
+        }
+	}
+}
